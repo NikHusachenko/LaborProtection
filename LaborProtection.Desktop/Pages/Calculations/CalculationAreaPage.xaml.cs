94c593349d69dcff322a75f32f705180075b04a9
NikHusachenko <nikgusachenko@gmail.com>
2023-04-19 12:38:02 +0300

    Designed CalculationPage

--- LaborProtection.Desktop/Pages/Calculations/CalculationAreaPage.xaml.cs
+++ LaborProtection.Desktop/Pages/Calculations/CalculationAreaPage.xaml.cs
@@ -15,3 +15,20 @@ namespace LaborProtection.Desktop.Pages.Calculations
         }
     }
 }
+
+// Work Are                             - 1
+// Work Volume                          - 2
+
+// Tables in width                      - 3
+// Tables in length                     - 4
+// Total tables count                   - 5
+
+// Lamps in width                       - 6
+// Lamps in length                      - 7
+// Total lamps count                    - 8
+
+// Bulbs in one lamp                    - 9
+// Total bulbs count                    - 10
+
+// Price on one lamp with bulbs         - 11
+// Total price                          - 12
