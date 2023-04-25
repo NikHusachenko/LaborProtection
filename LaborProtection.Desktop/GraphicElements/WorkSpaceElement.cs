a25f12c00006527c0ebbbefa45988b6f9a4e1e39
Oleg <olegredko45@gmail.com>
2023-04-25 18:21:40 +0300

    Add WorkSpaceElements. WorkSpaceWindow and LampWindow

---
+++ LaborProtection.Desktop/GraphicElements/WorkSpaceElement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Controls;
+
+namespace LaborProtection.Desktop.Elements
+{
+	public class WorkSpaceElement
+	{
+		public Rectangle MonitorElement { get; set; }
+		public Rectangle TableElement { get; set; }
+		public Rectangle WorkAreaElement { get; set; }
+	}
+}
