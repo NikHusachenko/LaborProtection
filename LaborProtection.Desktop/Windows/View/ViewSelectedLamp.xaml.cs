84b145f5bab685d012a15109ee19d2ba2d09a7fe
NikHusachenko <nikgusachenko@gmail.com>
2023-05-03 22:13:33 +0300

    Improvments of architecture

---
+++ LaborProtection.Desktop/Windows/View/ViewSelectedLamp.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace LaborProtection.Desktop.Windows.View
+{
+    public partial class ViewSelectedLamp : Window
+    {
+        public ViewSelectedLamp()
+        {
+            InitializeComponent();
+        }
+    }
+}
