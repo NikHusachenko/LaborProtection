f70f043683b6369fb12ab7499ca29d78cd2ae57b
Oleg <olegredko45@gmail.com>
2023-05-08 23:10:12 +0300

    Add displaying of LampElements and update WorkSpaceService

--- LaborProtection.Desktop/Pages/Calculations/CalculationAreaPage.xaml.cs
+++ LaborProtection.Desktop/Pages/Calculations/CalculationAreaPage.xaml.cs
@@ -196,15 +196,22 @@ namespace LaborProtection.Desktop.Pages.Calculations
 				Height = Convert.ToDouble(roomHeightTextBox.Text),
 				Table = tableEntity,
 			};
-			RoomWorkSpacesWindow roomWorkSpacesWindow = new RoomWorkSpacesWindow(new RoomEntity()
+			RoomEntity room = new RoomEntity()
 			{
 				Width = Convert.ToDouble(roomWidthTextBox.Text),
 				Length = Convert.ToDouble(roomLengthTextBox.Text),
 				Height = Convert.ToDouble(roomHeightTextBox.Text),
-                WorkSpace = workSpace,
-			});
+				WorkSpace = workSpace,
+			};
+			RoomWorkSpacesWindow roomWorkSpacesWindow = new RoomWorkSpacesWindow(room);
 			roomWorkSpacesWindow.ShowDialog();
 
+			double floorRefl = Convert.ToDouble(floorReflectionComboBox.SelectedItem);
+			double wallRefl = Convert.ToDouble(wallReflectionComboBox.SelectedItem);
+			double cellingRefl = Convert.ToDouble(cellingReflectionComboBox.SelectedItem);
+
+			RoomLampsWindow roomLampsWindow = new RoomLampsWindow(room, _lightService, _selectedLamp, _selectedBulb, floorRefl, wallRefl, cellingRefl, _selectedLamp.Type);
+			roomLampsWindow.ShowDialog();
 		}
 
 		private async void CalculationWorkArea()
@@ -264,8 +271,8 @@ namespace LaborProtection.Desktop.Pages.Calculations
             workLengthValueLabel.Content = spaceArea.Value.Length;
             workWidthValueLabel.Content = spaceArea.Value.Width;
 
-            int inLength = _workSpaceService.GetWorkSpacesInLegth(spaceArea.Value.Length, roomLength);
-            int inWidth = _workSpaceService.GetWorkSpacesInWidth(spaceArea.Value.Width, roomWidth);
+            int inWidth = _workSpaceService.GetWorkSpacesInLegth(spaceArea.Value.Length, roomLength);
+            int inLength = _workSpaceService.GetWorkSpacesInWidth(spaceArea.Value.Width, roomWidth);
             tablesInLengthValueLabel.Content = inLength;
             tablesInWidthValueLabel.Content = inWidth;
             totalTablesCountValueLabel.Content = inLength * inWidth;
--- LaborProtection.Desktop/RoomLampsWindow.xaml.cs
+++ LaborProtection.Desktop/RoomLampsWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using LaborProtection.Calculation.Entities;
 using LaborProtection.Database.Entities;
+using LaborProtection.Database.Enums;
+using LaborProtection.Desktop.GraphicElements;
+using LaborProtection.Services.LightServices;
+using LaborProtection.Services.TransponeServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +17,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static LaborProtection.Calculation.Constants.LightReflection;
+using System.Xaml;
+using LaborProtection.Services.Response;
 
 namespace LaborProtection.Desktop
 {
@@ -21,17 +28,40 @@ namespace LaborProtection.Desktop
 	/// </summary>
 	public partial class RoomLampsWindow : Window
 	{
+		private static TransponeService _transponeServiceWidth;
+		private static TransponeService _transponseSerivceHeight;
 		private readonly RoomEntity _roomEntity;
+		private readonly ILightService _lightService;
 		private readonly LampEntity _lampEntity;
-		public RoomLampsWindow()
+		private readonly BulbEntity _bulbEntity;
+		private readonly double _floorReflection;
+		private readonly double _ceillingReflection;
+		private readonly double _wallReflection;
+		private readonly LampType _lampType;
+		private static ResponseService<int> lampsNumber;
+
+		public RoomLampsWindow(RoomEntity room, ILightService lightService, LampEntity lampEntity, BulbEntity bulbEntity,
+			double floorReflection, double wallReflection, double ceillingReflection, LampType lampType)
 		{
+			_roomEntity = room;
+			_lightService = lightService;
+			_bulbEntity= bulbEntity;
+			_lampEntity= lampEntity;
+			_floorReflection = floorReflection;
+			_ceillingReflection = ceillingReflection;
+			_wallReflection = wallReflection;
+			_lampType = lampType;
+
 			InitializeComponent();
 		}
-
-		private void canvasGrid_Loaded(object sender, RoutedEventArgs e)
+		private async void canvasGrid_Loaded(object sender, RoutedEventArgs e)
 		{
+			lampsNumber = await _lightService.GetLampCount(_roomEntity, _lampEntity, _bulbEntity, _floorReflection, _wallReflection, _ceillingReflection, _lampType);
 			canvasGrid.Visibility = Visibility.Visible;
+			_transponeServiceWidth = new TransponeService(_roomEntity.Width, canvasGrid.ActualWidth);
+			_transponseSerivceHeight = new TransponeService(_roomEntity.Length, canvasGrid.ActualHeight);
 
+			RoomElement roomElement = new RoomElement(_roomEntity, _transponeServiceWidth, _transponseSerivceHeight, lampsNumber.Value, canvasGrid);
 		}
 	}
 }
